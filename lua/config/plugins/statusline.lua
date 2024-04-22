1b8a31cfd842fa4189b7deca24597636696a0763
Noahlias <yygggg@foxmail.com>
2024-04-22 11:47:57 +0800

    Change theme

--- lua/config/plugins/statusline.lua
+++ lua/config/plugins/statusline.lua
@@ -60,7 +60,7 @@ return {
 	opts = {
 		options = {
 			icons_enabled = true,
-			theme = 'tokyonight',
+			theme = 'auto',
 			component_separators = { left = '', right = '' },
 			section_separators = { left = '', right = '' },
 			disabled_filetypes = {
