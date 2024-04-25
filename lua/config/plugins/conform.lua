1e10eae5f0597e8fe7739d215f9b9aca3c3b5965
Noahlias <yygggg@foxmail.com>
2024-04-25 15:37:38 +0800

    Update python format

--- lua/config/plugins/conform.lua
+++ lua/config/plugins/conform.lua
@@ -1,46 +1,48 @@
 return {
-	'stevearc/conform.nvim',
-	opts = {
-		format_on_save = {
-			timeout_ms = 500,
-			lsp_fallback = true,
-		},
-		lua = { "stylua" },
-		python = { "black", "isort", "ruff" },
-		go = { "goimports", "gofmt" },
+  "stevearc/conform.nvim",
+  opts = {
+    format_on_save = {
+      timeout_ms = 500,
+      lsp_fallback = true,
+    },
+  },
+  config = function()
+    require("conform").setup {
+      format_on_save = {
+        -- These options will be passed to conform.format()
+        timeout_ms = 500,
+        lsp_fallback = true,
+      },
+      formatters_by_ft = {
+        lua = { "stylua" },
+        python = { "black", "isort", "ruff" },
+        go = { "goimports", "gofmt" },
 
-		html = { "prettier" },
-		css = { "prettier" },
-		less = { "prettier" },
-		scss = { "prettier" },
-		javascript = { "prettier" },
-		typescript = { "prettier" },
-		javascriptreact = { "prettier" },
-		typescriptreact = { "prettier" },
-		vue = { "prettier" },
-		json = { "prettier" },
-		yaml = { "prettier" },
-		markdown = { "markdownlint" }
-	},
-	config = function()
-		require("conform").setup(
-			{
-				format_on_save = {
-					-- These options will be passed to conform.format()
-					timeout_ms = 500,
-					lsp_fallback = true,
-				}
-			}
-		)
-	end,
-	-- keys = {
-	-- 	{
-	-- 		"<leader>cf",
-	-- 		function()
-	-- 			require("conform").format { lsp_fallback = true }
-	-- 		end,
-	-- 		desc = "Format Document",
-	-- 		mode = { "n", "v" }
-	-- 	},
-	-- },
+        html = { "prettier" },
+        css = { "prettier" },
+        less = { "prettier" },
+        scss = { "prettier" },
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        javascriptreact = { "prettier" },
+        typescriptreact = { "prettier" },
+        vue = { "prettier" },
+        json = { "prettier" },
+        yaml = { "prettier" },
+        markdown = { "markdownlint" },
+      },
+      -- NOTE: mayebe need to fix this with path variable  <04/25, 2024, noahlias> --
+      formatters = {
+        isort = {
+          command = "/Users/alias/.rye/shims/isort",
+        },
+        black = {
+          command = "/Users/alias/.rye/shims/black",
+        },
+        ruff = {
+          command = "/Users/alias/.rye/shims/ruff",
+        },
+      },
+    }
+  end,
 }
