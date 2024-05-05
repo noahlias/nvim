e5b1da5e19f14f40182697fc0fdead844ef473a7
Noahlias <yygggg@foxmail.com>
2024-05-05 08:38:43 +0800

    Add conceallevel for obsidian.nvim

--- lua/config/defaults.lua
+++ lua/config/defaults.lua
@@ -15,34 +15,34 @@ vim.o.shiftwidth = 2
 vim.o.softtabstop = 2
 vim.o.autoindent = true
 vim.o.list = true
-vim.o.listchars = 'tab:|\\ ,trail:▫'
+vim.o.listchars = "tab:|\\ ,trail:▫"
 vim.o.scrolloff = 4
 vim.o.ttimeoutlen = 0
 vim.o.timeout = false
-vim.o.viewoptions = 'cursor,folds,slash,unix'
+vim.o.viewoptions = "cursor,folds,slash,unix"
 vim.o.wrap = true
 vim.o.textwidth = 0
-vim.o.indentexpr = ''
-vim.o.foldmethod = 'indent'
+vim.o.indentexpr = ""
+vim.o.foldmethod = "indent"
 vim.o.foldlevel = 99
 vim.o.foldenable = true
 vim.o.foldlevelstart = 99
-vim.o.formatoptions = vim.o.formatoptions:gsub('tc', '')
+vim.o.formatoptions = vim.o.formatoptions:gsub("tc", "")
 vim.o.splitright = true
 vim.o.splitbelow = true
 vim.o.showmode = false
 vim.o.ignorecase = true
 vim.o.smartcase = true
-vim.o.shortmess = vim.o.shortmess .. 'c'
-vim.o.inccommand = 'split'
-vim.o.completeopt = 'longest,noinsert,menuone,noselect,preview'
-vim.o.completeopt = 'menuone,noinsert,noselect,preview'
+vim.o.shortmess = vim.o.shortmess .. "c"
+vim.o.inccommand = "split"
+vim.o.completeopt = "longest,noinsert,menuone,noselect,preview"
+vim.o.completeopt = "menuone,noinsert,noselect,preview"
 -- vim.o.lazyredraw = true
 vim.o.visualbell = true
-vim.o.colorcolumn = '100'
+vim.o.colorcolumn = "100"
 vim.o.updatetime = 100
-vim.o.virtualedit = 'block'
-vim.g.maplocalleader = '  '
+vim.o.virtualedit = "block"
+vim.g.maplocalleader = "  "
 
 -- local hr = tonumber(os.date('%H', os.time()))
 -- if hr > 6 and hr < 21 then -- day between 6am and 9pm
@@ -51,8 +51,9 @@ vim.g.maplocalleader = '  '
 -- 	vim.opt.background = 'dark'
 -- end
 
+vim.opt.conceallevel = 1
 -- set spelllang=en_us,cjk
-vim.cmd([[
+vim.cmd [[
 silent !mkdir -p $HOME/.config/nvim/tmp/backup
 silent !mkdir -p $HOME/.config/nvim/tmp/undo
 "silent !mkdir -p $HOME/.config/nvim/tmp/sessions
@@ -63,52 +64,54 @@ if has('persistent_undo')
 	set undofile
 	set undodir=$HOME/.config/nvim/tmp/undo,.
 endif
-]])
+]]
 
-vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" },
-	{ pattern = "*.md", command = "setlocal spell spelllang=en_us,cjk", })
-vim.api.nvim_create_autocmd("BufEnter", { pattern = "*", command = "silent! lcd %:p:h", })
+vim.api.nvim_create_autocmd(
+  { "BufRead", "BufNewFile" },
+  { pattern = "*.md", command = "setlocal spell spelllang=en_us,cjk" }
+)
+vim.api.nvim_create_autocmd("BufEnter", { pattern = "*", command = "silent! lcd %:p:h" })
 
-vim.cmd([[au BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif]])
+vim.cmd [[au BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif]]
 
-vim.g.terminal_color_0  = '#000000'
-vim.g.terminal_color_1  = '#FF5555'
-vim.g.terminal_color_2  = '#50FA7B'
-vim.g.terminal_color_3  = '#F1FA8C'
-vim.g.terminal_color_4  = '#BD93F9'
-vim.g.terminal_color_5  = '#FF79C6'
-vim.g.terminal_color_6  = '#8BE9FD'
-vim.g.terminal_color_7  = '#BFBFBF'
-vim.g.terminal_color_8  = '#4D4D4D'
-vim.g.terminal_color_9  = '#FF6E67'
-vim.g.terminal_color_10 = '#5AF78E'
-vim.g.terminal_color_11 = '#F4F99D'
-vim.g.terminal_color_12 = '#CAA9FA'
-vim.g.terminal_color_13 = '#FF92D0'
-vim.g.terminal_color_14 = '#9AEDFE'
-vim.cmd([[autocmd TermOpen term://* startinsert]])
-vim.cmd([[
+vim.g.terminal_color_0 = "#000000"
+vim.g.terminal_color_1 = "#FF5555"
+vim.g.terminal_color_2 = "#50FA7B"
+vim.g.terminal_color_3 = "#F1FA8C"
+vim.g.terminal_color_4 = "#BD93F9"
+vim.g.terminal_color_5 = "#FF79C6"
+vim.g.terminal_color_6 = "#8BE9FD"
+vim.g.terminal_color_7 = "#BFBFBF"
+vim.g.terminal_color_8 = "#4D4D4D"
+vim.g.terminal_color_9 = "#FF6E67"
+vim.g.terminal_color_10 = "#5AF78E"
+vim.g.terminal_color_11 = "#F4F99D"
+vim.g.terminal_color_12 = "#CAA9FA"
+vim.g.terminal_color_13 = "#FF92D0"
+vim.g.terminal_color_14 = "#9AEDFE"
+vim.cmd [[autocmd TermOpen term://* startinsert]]
+vim.cmd [[
 augroup NVIMRC
     autocmd!
     autocmd BufWritePost .vim.lua exec ":so %"
 augroup END
 tnoremap <C-N> <C-\><C-N>
 tnoremap <C-O> <C-\><C-N><C-O>
-]])
+]]
 
-vim.cmd([[hi NonText ctermfg=gray guifg=grey10]])
+vim.cmd [[hi NonText ctermfg=gray guifg=grey10]]
 
-local config_path = vim.fn.stdpath("config")
+local config_path = vim.fn.stdpath "config"
 local current_config_path = config_path .. "/lua/config/machine_specific.lua"
 if not vim.loop.fs_stat(current_config_path) then
-	local current_config_file = io.open(current_config_path, "wb")
-	local default_config_path = config_path .. "/default_config/_machine_specific_default.lua"
-	local default_config_file = io.open(default_config_path, "rb")
-	if default_config_file and current_config_file then
-		local content = default_config_file:read("*all")
-		current_config_file:write(content)
-		io.close(default_config_file)
-		io.close(current_config_file)
-	end
+  local current_config_file = io.open(current_config_path, "wb")
+  local default_config_path = config_path .. "/default_config/_machine_specific_default.lua"
+  local default_config_file = io.open(default_config_path, "rb")
+  if default_config_file and current_config_file then
+    local content = default_config_file:read "*all"
+    current_config_file:write(content)
+    io.close(default_config_file)
+    io.close(current_config_file)
+  end
 end
-require("config.machine_specific")
+require "config.machine_specific"
