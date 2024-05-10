f6de6e0d1ecf4f2819757e1b93fb89ff730c59cc
Noahlias <yygggg@foxmail.com>
2024-05-10 11:00:16 +0800

    Fix keymap conflict

--- lua/config/plugins/comment.lua
+++ lua/config/plugins/comment.lua
@@ -1,16 +1,16 @@
 return {
-	"tomtom/tcomment_vim",
-	event = "BufRead",
-	config = function()
-		vim.g.tcomment_maps = true
-		vim.g.tcomment_textobject_inlinecomment = ''
+  "tomtom/tcomment_vim",
+  event = "BufRead",
+  config = function()
+    vim.g.tcomment_maps = true
+    vim.g.tcomment_textobject_inlinecomment = ""
 
-		vim.cmd([[
-nmap <LEADER>cn g>c
-vmap <LEADER>cn g>
-nmap <LEADER>cu g<c
-vmap <LEADER>cu g<
-unmap ic
-		]])
-	end
+    -- 		vim.cmd([[
+    -- nmap <LEADER>cn g>c
+    -- vmap <LEADER>cn g>
+    -- nmap <LEADER>cu g<c
+    -- vmap <LEADER>cu g<
+    -- unmap ic
+    -- 		]])
+  end,
 }
