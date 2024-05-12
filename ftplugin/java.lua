1efa5c17ebf83a88981e3b70f7f5480c6b3e3d23
Noahlias <yygggg@foxmail.com>
2024-05-12 09:43:39 +0800

    Add java config

---
+++ ftplugin/java.lua
@@ -0,0 +1,44 @@
+local jdtls = require "jdtls"
+local mason = require "mason-registry"
+local capabilities = require "config.capabilities"
+
+local opts = {
+  cmd = { vim.fs.joinpath(mason.get_package("jdtls"):get_install_path(), "/bin/jdtls") },
+  handlers = {
+    ["language/status"] = function() end,
+  },
+  settings = {
+    java = {
+      inlayHints = {
+        parameterNames = {
+          enabled = "all",
+        },
+      },
+    },
+  },
+  capabilities = capabilities,
+  on_attach = function(client, bufnr)
+    jdtls.setup_dap { hotcodereplace = "auto" }
+    require("jdtls.setup").add_commands()
+  end,
+  init_options = {
+    bundles = {
+      vim.fn.glob(
+        vim.fs.joinpath(
+          mason.get_package("java-debug-adapter"):get_install_path(),
+          "extension/server/com.microsoft.java.debug.plugin-*.jar"
+        ),
+        1
+      ),
+    },
+  },
+}
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "java",
+  desc = "Attach jdtls",
+  callback = function()
+    jdtls.start_or_attach(opts)
+    vim.bo.tabstop = 4
+  end,
+})
