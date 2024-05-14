cb0f1b770b3ca6bd46374fddd87b454049db6f09
Noahlias <yygggg@foxmail.com>
2024-05-14 23:50:32 +0800

    Update comment

--- lua/config/plugins/comment.lua
+++ lua/config/plugins/comment.lua
@@ -1,16 +1,9 @@
 return {
-  "tomtom/tcomment_vim",
-  event = "BufRead",
-  config = function()
-    vim.g.tcomment_maps = true
-    vim.g.tcomment_textobject_inlinecomment = ""
-
-    -- 		vim.cmd([[
-    -- nmap <LEADER>cn g>c
-    -- vmap <LEADER>cn g>
-    -- nmap <LEADER>cu g<c
-    -- vmap <LEADER>cu g<
-    -- unmap ic
-    -- 		]])
-  end,
+  {
+    "echasnovski/mini.comment",
+    version = false,
+    config = function()
+      require("mini.comment").setup()
+    end,
+  },
 }
