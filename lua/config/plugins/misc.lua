333e4c4c1e5604cebafe177625833d443a43354d
Noahlias <yygggg@foxmail.com>
2024-05-15 13:04:18 +0800

    Add ascii digram plugin

--- lua/config/plugins/misc.lua
+++ lua/config/plugins/misc.lua
@@ -259,16 +259,16 @@ return {
       integrations = {
         markdown = {
           enabled = true,
-          clear_in_insert_mode = false,
+          clear_in_insert_mode = true,
           download_remote_images = true,
-          only_render_image_at_cursor = false,
+          only_render_image_at_cursor = true,
           filetypes = { "markdown", "vimwiki", "nvim" }, -- markdown extensions (ie. quarto) can go here
         },
         neorg = {
           enabled = true,
-          clear_in_insert_mode = false,
+          clear_in_insert_mode = true,
           download_remote_images = true,
-          only_render_image_at_cursor = false,
+          only_render_image_at_cursor = true,
           filetypes = { "norg" },
         },
       },
@@ -502,5 +502,36 @@ return {
         desc = "Interrupts the currently running cell and does nothing if not cell is running.",
       },
     },
+  },
+  --NOTE: add ascii diagram support
+  {
+    "jbyuki/venn.nvim",
+    config = function()
+      -- venn.nvim: enable or disable keymappings
+      function _G.Toggle_venn()
+        local venn_enabled = vim.inspect(vim.b.venn_enabled)
+        if venn_enabled == "nil" then
+          vim.b.venn_enabled = true
+          vim.cmd [[setlocal ve=all]]
+          -- draw a line on HJKL keystokes
+          vim.api.nvim_buf_set_keymap(0, "n", "J", "<C-v>j:VBox<CR>", { noremap = true })
+          vim.api.nvim_buf_set_keymap(0, "n", "K", "<C-v>k:VBox<CR>", { noremap = true })
+          vim.api.nvim_buf_set_keymap(0, "n", "L", "<C-v>l:VBox<CR>", { noremap = true })
+          vim.api.nvim_buf_set_keymap(0, "n", "H", "<C-v>h:VBox<CR>", { noremap = true })
+          -- draw a box by pressing "f" with visual selection
+          vim.api.nvim_buf_set_keymap(0, "v", "f", ":VBox<CR>", { noremap = true })
+        else
+          vim.cmd [[setlocal ve=]]
+          vim.api.nvim_buf_del_keymap(0, "n", "J")
+          vim.api.nvim_buf_del_keymap(0, "n", "K")
+          vim.api.nvim_buf_del_keymap(0, "n", "L")
+          vim.api.nvim_buf_del_keymap(0, "n", "H")
+          vim.api.nvim_buf_del_keymap(0, "v", "f")
+          vim.b.venn_enabled = nil
+        end
+      end
+      -- toggle keymappings for venn using <leader>v
+      vim.api.nvim_set_keymap("n", "<leader>v", ":lua Toggle_venn()<CR>", { noremap = true })
+    end,
   },
 }
