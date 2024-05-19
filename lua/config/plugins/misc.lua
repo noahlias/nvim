851112b1b587b1ef8bb07d7efee8b887ab26c133
Noahlias <yygggg@foxmail.com>
2024-05-19 12:53:40 +0800

    Fix conflict with jump and add silent

--- lua/config/plugins/misc.lua
+++ lua/config/plugins/misc.lua
@@ -524,23 +524,24 @@ return {
       -- venn.nvim: enable or disable keymappings
       function _G.Toggle_venn()
         local venn_enabled = vim.inspect(vim.b.venn_enabled)
+        local opts = { noremap = true, silent = true }
         if venn_enabled == "nil" then
           vim.b.venn_enabled = true
           vim.cmd [[setlocal ve=all]]
           -- draw a line on HJKL keystokes
-          vim.api.nvim_buf_set_keymap(0, "n", "J", "<C-v>j:VBox<CR>", { noremap = true })
-          vim.api.nvim_buf_set_keymap(0, "n", "K", "<C-v>k:VBox<CR>", { noremap = true })
-          vim.api.nvim_buf_set_keymap(0, "n", "L", "<C-v>l:VBox<CR>", { noremap = true })
-          vim.api.nvim_buf_set_keymap(0, "n", "H", "<C-v>h:VBox<CR>", { noremap = true })
+          vim.api.nvim_buf_set_keymap(0, "n", "J", "<C-v>j:VBox<CR>", opts)
+          vim.api.nvim_buf_set_keymap(0, "n", "K", "<C-v>k:VBox<CR>", opts)
+          vim.api.nvim_buf_set_keymap(0, "n", "L", "<C-v>l:VBox<CR>", opts)
+          vim.api.nvim_buf_set_keymap(0, "n", "H", "<C-v>h:VBox<CR>", opts)
           -- draw a box by pressing "f" with visual selection
-          vim.api.nvim_buf_set_keymap(0, "v", "f", ":VBox<CR>", { noremap = true })
+          vim.api.nvim_buf_set_keymap(0, "v", "v", ":VBox<CR>", { noremap = true })
         else
           vim.cmd [[setlocal ve=]]
           vim.api.nvim_buf_del_keymap(0, "n", "J")
           vim.api.nvim_buf_del_keymap(0, "n", "K")
           vim.api.nvim_buf_del_keymap(0, "n", "L")
           vim.api.nvim_buf_del_keymap(0, "n", "H")
-          vim.api.nvim_buf_del_keymap(0, "v", "f")
+          vim.api.nvim_buf_del_keymap(0, "v", "v")
           vim.b.venn_enabled = nil
         end
       end
