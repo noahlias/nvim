1e1e8a37417a96ff9cc5ecc25a93cbba846a883c
Noahlias <yygggg@foxmail.com>
2024-05-21 13:25:45 +0800

    Revert

--- lua/config/plugins/statusline.lua
+++ lua/config/plugins/statusline.lua
@@ -60,8 +60,8 @@ return {
   opts = {
     options = {
       icons_enabled = true,
-      -- theme = "gruvbox-material",
-      theme = "auto",
+      theme = "gruvbox-material",
+      -- theme = "auto",
       component_separators = { left = "", right = "" },
       section_separators = { left = "", right = "" },
       disabled_filetypes = {
