7a14bc9e86dcde47f2a9fd72d6a4933c3198e78a
Noahlias <yygggg@foxmail.com>
2024-05-23 16:09:48 +0800

    Very useful plugin to `quickfix`

--- lua/config/plugins/editor.lua
+++ lua/config/plugins/editor.lua
@@ -137,4 +137,22 @@ return {
       require("nvim-autopairs").setup {}
     end,
   },
+  {
+    "kevinhwang91/nvim-bqf",
+    ft = "qf",
+    dependencies = {
+      {
+        "junegunn/fzf",
+        build = function()
+          vim.fn["fzf#install"]()
+        end,
+      },
+    },
+    opts = {
+      auto_resize_height = true,
+      preview = {
+        border = "rounded",
+      },
+    },
+  },
 }
