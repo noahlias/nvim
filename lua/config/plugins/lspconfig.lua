dd6da0770a45fdcbdad5aa28d0ce90b3263b194b
Noahlias <yygggg@foxmail.com>
2024-05-26 18:57:37 +0800

    Remove unused

--- lua/config/plugins/lspconfig.lua
+++ lua/config/plugins/lspconfig.lua
@@ -136,37 +136,6 @@ M.config = {
   },
 }
 
--- F.configureInlayHints = function()
---   require("lsp-inlayhints").setup {
---     inlay_hints = {
---       parameter_hints = {
---         show = true,
---         prefix = "<- ",
---         separator = ", ",
---         remove_colon_start = false,
---         remove_colon_end = true,
---       },
---       type_hints = {
---         -- type and other hints
---         show = true,
---         prefix = "",
---         separator = ", ",
---         remove_colon_start = false,
---         remove_colon_end = false,
---       },
---       only_current_line = false,
---       -- separator between types and parameter hints. Note that type hints are
---       -- shown before parameter
---       labels_separator = "  ",
---       -- whether to align to the length of the longest line in the file
---       max_len_align = false,
---       -- padding from the left if max_len_align is true
---       max_len_align_padding = 1,
---       highlight = "Comment",
---     },
---   }
--- end
-
 F.configureDocAndSignature = function()
   vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
     -- silent = true,
