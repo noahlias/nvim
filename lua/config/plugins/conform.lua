efb7f3daf47cd2fda1ef4de3966db83afd1a1e28
Noahlias <yygggg@foxmail.com>
2024-05-27 11:59:55 +0800

    Not autoformat on `c or cpp`

--- lua/config/plugins/conform.lua
+++ lua/config/plugins/conform.lua
@@ -9,11 +9,16 @@ return {
   },
   config = function()
     require("conform").setup {
-      format_on_save = {
-        -- These options will be passed to conform.format()
-        timeout_ms = 500,
-        lsp_fallback = true,
-      },
+      format_on_save = function(bufnr)
+        local ignore_filetypes = { "c", "cpp" }
+        if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
+          return
+        end
+        return {
+          timeout_ms = 500,
+          lsp_fallback = true,
+        }
+      end,
       formatters_by_ft = {
         lua = { "stylua" },
         python = function(bufnr)
