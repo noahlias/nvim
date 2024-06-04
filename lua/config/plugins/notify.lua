c89430c6953f6be0214a0007ec2445ea2defef68
Noahlias <yygggg@foxmail.com>
2024-06-05 00:13:40 +0800

    Update `notify` config

--- lua/config/plugins/notify.lua
+++ lua/config/plugins/notify.lua
@@ -4,30 +4,36 @@ return {
   config = function()
     local notify = require "notify"
     vim.notify = notify
+
+    ---@diagnostic disable-next-line: missing-fields
     notify.setup {
       on_open = function(win)
         vim.api.nvim_win_set_config(win, { zindex = 100 })
       end,
-      background_colour = "#202020",
+      background_color = "#202020",
       minimum_width = 50,
-      render = "compact",
+      render = "wrapped-compact",
       stages = "static",
       timeout = 3000,
       top_down = true,
     }
-    local opts = { noremap = true, silent = true }
     vim.keymap.set("n", ",;", function()
       require("telescope").extensions.notify.notify {
         layout_strategy = "vertical",
         layout_config = {
-          width = 0.9,
-          height = 0.9,
+          width = 0.7,
+          height = 0.8,
           -- preview_height = 0.1,
         },
         wrap_results = true,
         previewer = false,
       }
-    end, opts)
-    vim.keymap.set("n", "<LEADER>c;", notify.dismiss, opts)
+    end, { noremap = true, silent = true, desc = "Notify Telescope" })
+    vim.keymap.set(
+      "n",
+      "<LEADER>c;",
+      notify.dismiss,
+      { noremap = true, silent = true, desc = "Notify dismiss" }
+    )
   end,
 }
