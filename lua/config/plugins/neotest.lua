da2dfcf94703d5ffed545c012b82a5644ffe4f85
Noahlias <yygggg@foxmail.com>
2024-06-05 11:38:19 +0800

    Add `neotest`

---
+++ lua/config/plugins/neotest.lua
@@ -0,0 +1,90 @@
+---@type LazyPluginSpec
+return {
+  "rcarriga/neotest",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+
+    -- Adapters
+    -- "nvim-neotest/neotest-jest",
+    "nvim-neotest/neotest-go",
+    "nvim-neotest/neotest-python",
+    "marilari88/neotest-vitest",
+    "lawrence-laz/neotest-zig",
+  },
+  opts = function()
+    return {
+      adapters = {
+        -- require "neotest-jest",
+        require "neotest-go",
+        require "neotest-python" {
+          dap = {
+            -- justMyCode = false,
+          },
+        },
+        require "neotest-zig" {
+          dap = {
+            adapter = "lldb",
+          },
+        },
+        require "neotest-vitest" {
+          ---@diagnostic disable-next-line: unused-local
+          filter_dir = function(name, rel_path, root)
+            return name ~= "node_modules"
+          end,
+        },
+      },
+      consumers = {
+        overseer = require "neotest.consumers.overseer",
+      },
+    }
+  end,
+  config = function(_, opts)
+    require("neotest").setup(opts)
+  end,
+  keys = {
+    {
+      "<leader>Tr",
+      function()
+        require("neotest").run.run()
+      end,
+      desc = "Run",
+    },
+    {
+      "<leader>Ts",
+      function()
+        require("neotest").run.stop()
+      end,
+      desc = "Stop",
+    },
+    {
+      "<leader>Ta",
+      function()
+        require("neotest").run.attach()
+      end,
+      desc = "Attach",
+    },
+
+    {
+      "<leader>Tm",
+      function()
+        require("neotest").summary.toggle()
+      end,
+      desc = "Summary",
+    },
+    {
+      "<leader>Tw",
+      function()
+        require("neotest").watch.toggle()
+      end,
+      desc = "Watch",
+    },
+    {
+      "<leader>To",
+      function()
+        require("neotest").output_panel.toggle()
+      end,
+      desc = "Output panel",
+    },
+  },
+}
