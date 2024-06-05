a74be30210645617bc9275b9317926445020e8a1
Noahlias <yygggg@foxmail.com>
2024-06-06 01:01:16 +0800

    Update haskell config

--- after/ftplugin/haskell.lua
+++ after/ftplugin/haskell.lua
@@ -1,18 +1,50 @@
 -- ~/.config/nvim/after/ftplugin/haskell.lua
-local ht = require('haskell-tools')
+local ht = require "haskell-tools"
 local bufnr = vim.api.nvim_get_current_buf()
-local opts = { noremap = true, silent = true, buffer = bufnr, }
+local opts = { noremap = true, silent = true, buffer = bufnr }
+
+--- @param _opts table
+--- @param desc string
+local extend_desc = function(_opts, desc)
+  return vim.tbl_extend("force", _opts, { desc = desc })
+end
+
 -- haskell-language-server relies heavily on codeLenses,
 -- so auto-refresh (see advanced configuration) is enabled by default
-vim.keymap.set('n', '<space>cl', vim.lsp.codelens.run, opts)
+vim.keymap.set(
+  "n",
+  "<leader>hl",
+  vim.lsp.codelens.run,
+  extend_desc(opts, "Haskell LSP code lenses")
+)
 -- Hoogle search for the type signature of the definition under the cursor
-vim.keymap.set('n', '<space>hs', ht.hoogle.hoogle_signature, opts)
+vim.keymap.set(
+  "n",
+  "<leader>hs",
+  ht.hoogle.hoogle_signature,
+  extend_desc(opts, "Hoogle signature search")
+)
 -- Evaluate all code snippets
-vim.keymap.set('n', '<space>ea', ht.lsp.buf_eval_all, opts)
+vim.keymap.set(
+  "n",
+  "<leader>ha",
+  ht.lsp.buf_eval_all,
+  extend_desc(opts, "Evaluate all code snippets")
+)
 -- Toggle a GHCi repl for the current package
-vim.keymap.set('n', '<leader>rr', ht.repl.toggle, opts)
+vim.keymap.set(
+  "n",
+  "<leader>hr",
+  ht.repl.toggle,
+  extend_desc(opts, "Toggle a GHCi repl for the current package")
+)
 -- Toggle a GHCi repl for the current buffer
-vim.keymap.set('n', '<leader>rf', function()
-	ht.repl.toggle(vim.api.nvim_buf_get_name(0))
-end, opts)
-vim.keymap.set('n', '<leader>rq', ht.repl.quit, opts)
+vim.keymap.set("n", "<leader>hf", function()
+  ht.repl.toggle(vim.api.nvim_buf_get_name(0))
+end, extend_desc(opts, "Toggle a GHCi repl for the current buffer"))
+vim.keymap.set(
+  "n",
+  "<leader>hq",
+  ht.repl.quit,
+  extend_desc(opts, "Quit GHCi repl")
+)
