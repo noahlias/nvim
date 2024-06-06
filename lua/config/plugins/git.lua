e0e004055b7aaf26d6ecb6718c71f44a893b35f9
Noahlias <yygggg@foxmail.com>
2024-06-06 10:47:56 +0800

    gitsign config update

--- lua/config/plugins/git.lua
+++ lua/config/plugins/git.lua
@@ -6,25 +6,80 @@ return {
     config = function()
       require("gitsigns").setup {
         signs = {
-          add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-          change = { hl = "GitSignsChange", text = "░", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-          delete = { hl = "GitSignsDelete", text = "_", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-          topdelete = { hl = "GitSignsDelete", text = "▔", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+          add = {
+            hl = "GitSignsAdd",
+            text = "▎",
+            numhl = "GitSignsAddNr",
+            linehl = "GitSignsAddLn",
+          },
+          change = {
+            hl = "GitSignsChange",
+            text = "░",
+            numhl = "GitSignsChangeNr",
+            linehl = "GitSignsChangeLn",
+          },
+          delete = {
+            hl = "GitSignsDelete",
+            text = "_",
+            numhl = "GitSignsDeleteNr",
+            linehl = "GitSignsDeleteLn",
+          },
+          topdelete = {
+            hl = "GitSignsDelete",
+            text = "▔",
+            numhl = "GitSignsDeleteNr",
+            linehl = "GitSignsDeleteLn",
+          },
           changedelete = {
             hl = "GitSignsChange",
             text = "▒",
             numhl = "GitSignsChangeNr",
             linehl = "GitSignsChangeLn",
           },
-          untracked = { hl = "GitSignsAdd", text = "┆", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+          untracked = {
+            hl = "GitSignsAdd",
+            text = "┆",
+            numhl = "GitSignsAddNr",
+            linehl = "GitSignsAddLn",
+          },
         },
         -- current_line_blame = false,
+        word_diff = true,
+        attach_to_untracked = true,
+        preview_config = {
+          border = "rounded",
+        },
       }
-      vim.keymap.set("n", "<leader>g-", ":Gitsigns prev_hunk<CR>", { noremap = true, silent = true })
-      vim.keymap.set("n", "<leader>g=", ":Gitsigns next_hunk<CR>", { noremap = true, silent = true })
-      vim.keymap.set("n", "<leader>gb", ":Gitsigns blame_line<CR>", { noremap = true, silent = true })
-      vim.keymap.set("n", "<leader>gr", ":Gitsigns reset_hunk<CR>", { noremap = true, silent = true })
-      vim.keymap.set("n", "H", ":Gitsigns preview_hunk_inline<CR>", { noremap = true, silent = true })
+      vim.keymap.set(
+        "n",
+        "<leader>g-",
+        ":Gitsigns prev_hunk<CR>",
+        { noremap = true, silent = true }
+      )
+      vim.keymap.set(
+        "n",
+        "<leader>g=",
+        ":Gitsigns next_hunk<CR>",
+        { noremap = true, silent = true }
+      )
+      vim.keymap.set(
+        "n",
+        "<leader>gb",
+        ":Gitsigns blame_line<CR>",
+        { noremap = true, silent = true }
+      )
+      vim.keymap.set(
+        "n",
+        "<leader>gr",
+        ":Gitsigns reset_hunk<CR>",
+        { noremap = true, silent = true }
+      )
+      vim.keymap.set(
+        "n",
+        "H",
+        ":Gitsigns preview_hunk_inline<CR>",
+        { noremap = true, silent = true }
+      )
     end,
   },
   {
@@ -34,7 +89,12 @@ return {
       vim.g.lazygit_floating_window_scaling_factor = 1.0
       vim.g.lazygit_floating_window_winblend = 0
       vim.g.lazygit_use_neovim_remote = true
-      vim.keymap.set("n", "<leader>gl", ":LazyGit<CR>", { noremap = true, silent = true })
+      vim.keymap.set(
+        "n",
+        "<leader>gl",
+        ":LazyGit<CR>",
+        { noremap = true, silent = true }
+      )
     end,
   },
   {
