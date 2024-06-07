03b719f5058ff3a52658708d2269a96d16aa1f8f
Noahlias <yygggg@foxmail.com>
2024-06-07 09:40:21 +0800

    Move some filetype setting to this

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -1,1 +1,7 @@
 vim.cmd "setlocal colorcolumn=89"
+
+vim.bo.shiftwidth = 4
+vim.bo.softtabstop = 4
+vim.bo.expandtab = true
+
+vim.cmd [[ autocmd FileType python setlocal textwidth=80 wrap linebreak ]]
