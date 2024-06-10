e42d58dc788cb2f6603fc9a175545317488ddcec
Noahlias <yygggg@foxmail.com>
2024-06-11 00:27:38 +0800

    Update the plugin

--- lua/config/plugins/indent.lua
+++ lua/config/plugins/indent.lua
@@ -3,20 +3,16 @@ return {
   {
     "shellRaining/hlchunk.nvim",
     enabled = true,
-    -- branch = "dev",
-    commit = "a7cb0da6286156c4e601c5d0f47a82bda25b37a9",
+    branch = "dev",
     event = { "BufReadPre", "BufNewFile" },
     config = function()
       require("hlchunk").setup {
-
         chunk = {
           enable = true,
           style = {
             { fg = "#806d9c" },
             { fg = "#c21f30" },
           },
-          --NOTE: Maybe this is useful
-          textobject = "",
         },
         indent = {
           enable = true,
