3dd4db7dc192eaf50f482499284f3f7c6e5f26a3
Noahlias <yygggg@foxmail.com>
2024-06-13 10:24:36 +0800

    octo update

--- lua/config/plugins/octo.lua
+++ lua/config/plugins/octo.lua
@@ -2,7 +2,11 @@
 return {
   "pwntester/octo.nvim",
   cmd = { "Octo" },
+  event = { { event = "BufReadCmd", pattern = "octo://*" } },
   opts = {
+    enable_builtin = true,
+    default_to_projects_v2 = true,
+    default_merge_method = "squash",
     picker = "fzf-lua",
     picker_config = {
       use_emojis = true,
