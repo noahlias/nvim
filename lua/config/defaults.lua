6acab54d4b65b0b7fdc5d64798bde5039cb260b6
Noahlias <yygggg@foxmail.com>
2024-06-15 09:41:36 +0800

    Add `codeblock` copy

--- lua/config/defaults.lua
+++ lua/config/defaults.lua
@@ -106,3 +106,12 @@ vim.g.flutter_run_args = ""
 
 vim.g.codelldb_path =
   "/Users/alias/.local/share/nvim/dapinstall/codelldb/extension/adapter/codelldb"
+
+-- Copy text to clipboard using codeblock format ```{ft}{content}```
+vim.api.nvim_create_user_command("CopyCodeBlock", function(opts)
+  local lines = vim.api.nvim_buf_get_lines(0, opts.line1 - 1, opts.line2, true)
+  local content = table.concat(lines, "\n")
+  local result = string.format("```%s\n%s\n```", vim.bo.filetype, content)
+  vim.fn.setreg("+", result)
+  vim.notify "Text copied to clipboard"
+end, { range = true })
