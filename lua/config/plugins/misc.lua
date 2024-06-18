a8f342821516c97041ce6e76af5f2091499669d0
Noahlias <yygggg@foxmail.com>
2024-06-18 21:36:09 +0800

    Add screenkey

--- lua/config/plugins/misc.lua
+++ lua/config/plugins/misc.lua
@@ -609,4 +609,13 @@ return {
       },
     },
   },
+  {
+    "NStefan002/screenkey.nvim",
+    cmd = "Screenkey",
+    opts = {
+      win_opts = {
+        border = "rounded",
+      },
+    },
+  },
 }
