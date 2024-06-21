8352114799883676107762912bbb9bc79c849b6b
Noahlias <yygggg@foxmail.com>
2024-06-21 11:10:20 +0800

    update gitblame for statusline

--- lua/config/plugins/statusline.lua
+++ lua/config/plugins/statusline.lua
@@ -77,7 +77,12 @@ return {
       component_separators = { left = "", right = "" },
       section_separators = { left = "", right = "" },
       disabled_filetypes = {
-        statusline = { "alpha", "OverseerList", "DiffviewFiles" },
+        statusline = {
+          "alpha",
+          "OverseerList",
+          "DiffviewFiles",
+          "gitsigns.blame",
+        },
       },
       always_divide_middle = true,
       globalstatus = false,
