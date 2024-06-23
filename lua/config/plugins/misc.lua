edc348e09edc432efd49623f3a2ed8bb2c444fe4
Noahlias <yygggg@foxmail.com>
2024-06-23 13:01:06 +0800

    Remove kitty scrollback

--- lua/config/plugins/misc.lua
+++ lua/config/plugins/misc.lua
@@ -297,52 +297,6 @@ return {
     "dstein64/vim-startuptime",
     cmd = { "StartupTime" },
   },
-  -- {
-  --   "mikesmithgh/kitty-scrollback.nvim",
-  --   cmd = {
-  --     "KittyScrollbackGenerateKittens",
-  --     "KittyScrollbackCheckHealth",
-  --   },
-  --   event = { "User KittyScrollbackLaunch" },
-  --   opts = {
-  --     {
-  --       paste_window = {
-  --         winopts_overrides = function(winopts)
-  --           winopts.border = {
-  --             "╭",
-  --             "─",
-  --             "╮",
-  --             "│",
-  --             "┤",
-  --             "─",
-  --             "├",
-  --             "│",
-  --           }
-  --           return winopts
-  --         end,
-  --         footer_winopts_overrides = function(winopts)
-  --           winopts.border = {
-  --             "│",
-  --             " ",
-  --             "│",
-  --             "│",
-  --             "╯",
-  --             "─",
-  --             "╰",
-  --             "│",
-  --           }
-  --           return winopts
-  --         end,
-  --       },
-  --     },
-  --   },
-  --   callbacks = {
-  --     after_paste_window_ready = function(pastewin_data)
-  --       local winid = pastewin_data.paste_window.winid
-  --       vim.wo[winid].statuscolumn = ""
-  --     end,
-  --   },
-  -- },
   {
     "utilyre/sentiment.nvim",
     event = "VeryLazy", -- keep for lazy loading
