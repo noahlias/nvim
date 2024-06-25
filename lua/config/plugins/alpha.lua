db9beaa9fcc374d69d952644d1c5af656a9855e3
Noahlias <yygggg@foxmail.com>
2024-06-25 20:51:52 +0800

    Update git-dashboard config

--- lua/config/plugins/alpha.lua
+++ lua/config/plugins/alpha.lua
@@ -1,6 +1,14 @@
 ---@type LazyPluginSpec
 return {
   "goolord/alpha-nvim",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+    "nvim-lua/plenary.nvim",
+    {
+      "juansalvatore/git-dashboard-nvim",
+      dependencies = { "nvim-lua/plenary.nvim" },
+    },
+  },
   event = "VimEnter",
   init = function()
     --- NOTE: This disable the tabline in dashboard load screen.
@@ -21,32 +29,69 @@ return {
   end,
   config = function()
     local alpha = require "alpha"
-
-    local dashboard = require "alpha.themes.dashboard"
-    local theta = require "alpha.themes.theta"
-
-    theta.header.val = {
+    local default_dashboard = {
       [[                                                     ]],
       [[ ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗  ]],
       [[ ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║  ]],
       [[ ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║  ]],
       [[ ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║  ]],
       [[ ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║  ]],
-      -- [[⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡤⢀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀]],
-      -- [[⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠤⠙⣅⢩⣤⣴⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀]],
-      -- [[⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⣶⡾⢋⣉⣽⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀]],
-      -- [[⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣏⡿⣫⠥⠌⢸⣿⠇⠠⠀⢀⡀⠀⠀⠀⠀]],
-      -- [[⠀⠀⠀⠀⢠⡄⠀⠀⠀⠀⣿⣷⣤⣥⣒⣶⣿⡏⠀⠀⠀⣿⠇⠀⠀⠀⠀]],
-      -- [[⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠸⣿⣿⣿⡿⠛⠉⠀⠀⠀⠀⣿⡄⠀⠀⠀⠀]],
-      -- [[⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠘⠉⠉⠀⠀⠀⠀⠀⠀⢰⣿⣷⣤⣤⡀⠀]],
-      -- [[⠀⠀⠀⢠⣿⣯⡀⠀⠀⠀⠀⠀⢶⣶⣶⡆⣤⣴⣦⠀⣾⣿⣿⣿⣿⣅⠆]],
-      -- [[⠀⠀⠀⣿⣿⣿⠿⠀⠀⠀⠀⠀⠈⢽⣿⣿⣿⣿⠃⡠⣿⣿⣿⣛⣻⠛⡀]],
-      -- [[⠀⠀⢸⣿⣿⣿⡟⣧⠀⠀⠀⠀⠀⠈⢿⣿⣿⠋⠀⣰⣿⣿⣟⡉⠉⠉⢀]],
-      -- [[⠀⠀⠀⣿⣿⣷⡶⠃⠧⠀⠀⠀⠀⠀⠀⠉⠁⠀⠀⠁⠀⠙⠟⠉⠁⢠⣿]],
-      -- [[⠀⠀⠀⢻⣷⡶⠄⠀⠀⠐⠒⠒⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣠]],
-      -- [[⠀⠀⠀⠈⠳⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠟]],
+      --   -- [[⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡤⢀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀]],
+      --   -- [[⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠤⠙⣅⢩⣤⣴⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀]],
+      --   -- [[⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⣶⡾⢋⣉⣽⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀]],
+      --   -- [[⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣏⡿⣫⠥⠌⢸⣿⠇⠠⠀⢀⡀⠀⠀⠀⠀]],
+      --   -- [[⠀⠀⠀⠀⢠⡄⠀⠀⠀⠀⣿⣷⣤⣥⣒⣶⣿⡏⠀⠀⠀⣿⠇⠀⠀⠀⠀]],
+      --   -- [[⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠸⣿⣿⣿⡿⠛⠉⠀⠀⠀⠀⣿⡄⠀⠀⠀⠀]],
+      --   -- [[⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠘⠉⠉⠀⠀⠀⠀⠀⠀⢰⣿⣷⣤⣤⡀⠀]],
+      --   -- [[⠀⠀⠀⢠⣿⣯⡀⠀⠀⠀⠀⠀⢶⣶⣶⡆⣤⣴⣦⠀⣾⣿⣿⣿⣿⣅⠆]],
+      --   -- [[⠀⠀⠀⣿⣿⣿⠿⠀⠀⠀⠀⠀⠈⢽⣿⣿⣿⣿⠃⡠⣿⣿⣿⣛⣻⠛⡀]],
+      --   -- [[⠀⠀⢸⣿⣿⣿⡟⣧⠀⠀⠀⠀⠀⠈⢿⣿⣿⠋⠀⣰⣿⣿⣟⡉⠉⠉⢀]],
+      --   -- [[⠀⠀⠀⣿⣿⣷⡶⠃⠧⠀⠀⠀⠀⠀⠀⠉⠁⠀⠀⠁⠀⠙⠟⠉⠁⢠⣿]],
+      --   -- [[⠀⠀⠀⢻⣷⡶⠄⠀⠀⠐⠒⠒⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣠]],
+      --   -- [[⠀⠀⠀⠈⠳⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠟]],
+    }
+    local dashboard = require "alpha.themes.dashboard"
+    local theta = require "alpha.themes.theta"
+    --- @diagnostic disable-next-line:missing-fields
+    local git_dashboard = require("git-dashboard-nvim").setup {
+      centered = false,
+      top_padding = 0,
+      use_git_username_as_author = true,
+      bottom_padding = 0,
+      show_current_branch = false,
+      hide_cursor = true,
+      title = "owner_with_repo_name",
+      days = { "s", "m", "t", "w", "t", "f", "s" },
+      colors = {
+        --catpuccin theme
+        days_and_months_labels = "#8FBCBB",
+        empty_square_highlight = "#3B4252",
+        filled_square_highlights = {
+          "#88C0D0",
+          "#88C0D0",
+          "#88C0D0",
+          "#88C0D0",
+          "#88C0D0",
+          "#88C0D0",
+          "#88C0D0",
+        },
+        branch_highlight = "#88C0D0",
+        dashboard_title = "#88C0D0",
+      },
     }
 
+    --fallback to default if not git directory and git dashboard is not empty
+    local header = function()
+      if
+        vim.fn.isdirectory ".git" == 1
+        and table.concat(git_dashboard, "") ~= ""
+      then
+        return git_dashboard
+      else
+        return default_dashboard
+      end
+    end
+    theta.header.val = header
     theta.buttons.val = {
       {
         type = "text",
@@ -63,11 +108,6 @@ return {
         "c",
         "  Configuration",
         "<Cmd>edit ~/.config/nvim/init.lua<CR>"
-      ),
-      dashboard.button(
-        "f",
-        "  File Browser",
-        "<Cmd>Telescope file_browser<CR>"
       ),
       dashboard.button("d", "  Database", "<Cmd>DBUIToggle<CR>"),
       dashboard.button("q", "󰅚  Quit", "<Cmd>qa<CR>"),
