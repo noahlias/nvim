25d32c46420bbdcb211ebf4001908d239214318c
Noahlias <yygggg@foxmail.com>
2024-06-25 11:05:59 +0800

    Remove unused plugins

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -101,7 +101,6 @@ require("lazy").setup {
     require "config.plugins.jdtls",
     require "config.plugins.sql",
     require "config.plugins.comment",
-    require "config.plugins.rest",
     require "config.plugins.octo",
     require "config.plugins.tailwind",
     -- NOTE: This plugin use for my personal use
