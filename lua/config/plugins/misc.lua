7968175123521620e8d04fdeca21381ec8c948a2
Noahlias <yygggg@foxmail.com>
2024-06-27 11:19:23 +0800

    Update config

--- lua/config/plugins/misc.lua
+++ lua/config/plugins/misc.lua
@@ -311,6 +311,9 @@ return {
   {
     "dstein64/vim-startuptime",
     cmd = { "StartupTime" },
+    init = function()
+      vim.g.startuptime_tries = 10
+    end,
   },
   {
     "utilyre/sentiment.nvim",
