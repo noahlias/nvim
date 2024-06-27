698af8b558d990f2f13990c0c41b78a53788c4ee
Noahlias <yygggg@foxmail.com>
2024-06-28 01:39:46 +0800

    When too much commit it' becomes so slow

--- lua/config/plugins/alpha.lua
+++ lua/config/plugins/alpha.lua
@@ -8,6 +8,7 @@ return {
       dir = "~/.config/nvim/dev/git-dashboard-nvim",
       name = "git-dashboard-nvim",
       dev = true,
+      enabled = false,
     },
   },
   event = "VimEnter",
@@ -54,45 +55,45 @@ return {
     local dashboard = require "alpha.themes.dashboard"
     local theta = require "alpha.themes.theta"
     --- @diagnostic disable-next-line:missing-fields
-    local git_dashboard = require("git-dashboard-nvim").setup {
-      centered = false,
-      top_padding = 0,
-      use_git_username_as_author = true,
-      bottom_padding = 0,
-      show_current_branch = false,
-      hide_cursor = true,
-      title = "owner_with_repo_name",
-      days = { "s", "m", "t", "w", "t", "f", "s" },
-      colors = {
-        --catpuccin theme
-        days_and_months_labels = "#8FBCBB",
-        empty_square_highlight = "#3B4252",
-        filled_square_highlights = {
-          "#88C0D0",
-          "#88C0D0",
-          "#88C0D0",
-          "#88C0D0",
-          "#88C0D0",
-          "#88C0D0",
-          "#88C0D0",
-        },
-        branch_highlight = "#88C0D0",
-        dashboard_title = "#88C0D0",
-      },
-    }
+    -- local git_dashboard = require("git-dashboard-nvim").setup {
+    --   centered = false,
+    --   top_padding = 0,
+    --   use_git_username_as_author = true,
+    --   bottom_padding = 0,
+    --   show_current_branch = false,
+    --   hide_cursor = true,
+    --   title = "owner_with_repo_name",
+    --   days = { "s", "m", "t", "w", "t", "f", "s" },
+    --   colors = {
+    --     --catpuccin theme
+    --     days_and_months_labels = "#8FBCBB",
+    --     empty_square_highlight = "#3B4252",
+    --     filled_square_highlights = {
+    --       "#88C0D0",
+    --       "#88C0D0",
+    --       "#88C0D0",
+    --       "#88C0D0",
+    --       "#88C0D0",
+    --       "#88C0D0",
+    --       "#88C0D0",
+    --     },
+    --     branch_highlight = "#88C0D0",
+    --     dashboard_title = "#88C0D0",
+    --   },
+    -- }
 
     --fallback to default if not git directory and git dashboard is not empty
-    local header = function()
-      if
-        vim.fn.isdirectory ".git" == 1
-        and table.concat(git_dashboard, "") ~= ""
-      then
-        return git_dashboard
-      else
-        return default_dashboard
-      end
-    end
-    theta.header.val = header
+    -- local header = function()
+    --   if
+    --     vim.fn.isdirectory ".git" == 1
+    --     and table.concat(git_dashboard, "") ~= ""
+    --   then
+    --     return git_dashboard
+    --   else
+    --     return default_dashboard
+    --   end
+    -- end
+    theta.header.val = default_dashboard
     theta.buttons.val = {
       {
         type = "text",
