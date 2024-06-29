3076d60a3b8cdb2372d32fe378d350173fbaaf4c
Noahlias <yygggg@foxmail.com>
2024-06-29 12:05:03 +0800

    Fix latexmk out_dir bug

--- lua/config/plugins/misc.lua
+++ lua/config/plugins/misc.lua
@@ -468,12 +468,9 @@ return {
       vim.g.vimtex_mappings_prefix = "<localleader>"
       vim.g.vimtex_quickfix_mode = 0
       vim.g.vimtex_compiler_latexmk_engines = { _ = "-xelatex" }
-      vim.g.vimtex_compiler_latexrun_engines = { _ = "xelatex" }
       vim.g.vimtex_compiler_latexmk = {
-        build_dir = "build",
-        clean_on_exit = 1,
+        out_dir = "build",
       }
-      vim.g.vimtex_compiler_progname = "nvr"
       vim.g.vimtex_toc_config = {
         name = "TOC",
         layers = { "content", "todo", "include" },
