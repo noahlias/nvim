690c7c16150bf0b99c3de57cf7709b84b6b2988f
Noahlias <yygggg@foxmail.com>
2024-07-01 15:18:11 +0800

    Remove any jump

--- lua/config/plugins/search.lua
+++ lua/config/plugins/search.lua
@@ -48,18 +48,6 @@ return {
       )
     end,
   },
-  -- {
-  --   "pechorin/any-jump.vim",
-  --   event = "BufRead",
-  --   enabled = false,
-  --   config = function()
-  --     vim.keymap.set("n", "j", ":AnyJump<CR>", { noremap = true })
-  --     vim.keymap.set("x", "j", ":AnyJumpVisual<CR>", { noremap = true })
-  --     vim.g.any_jump_disable_default_keybindings = true
-  --     vim.g.any_jump_window_width_ratio = 0.9
-  --     vim.g.any_jump_window_height_ratio = 0.9
-  --   end,
-  -- },
   {
     "chrisgrieser/nvim-rip-substitute",
     event = "BufRead",
