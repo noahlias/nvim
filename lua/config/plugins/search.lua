7d8c936dbebedefe141e45636b26a35ee68741cd
Noahlias <yygggg@foxmail.com>
2024-07-04 18:13:33 +0800

    Revert

--- lua/config/plugins/search.lua
+++ lua/config/plugins/search.lua
@@ -3,67 +3,49 @@ return {
   {
     "kevinhwang91/nvim-hlslens",
     event = { "BufReadPre", "BufNewFile" },
-    keys = {
-      {
-        "n",
-        "g*",
-        function()
-          require("hlslens").start()
-        end,
-        { noremap = true, silent = true },
-      },
-      {
-        "n",
-        "g#",
-        function()
-          require("hlslens").start()
-        end,
-        { noremap = true, silent = true },
-      },
-    },
     config = function()
       require("hlslens").setup {
         build_position_cb = function(plist, _, _, _)
           require("scrollbar.handlers.search").handler.show(plist.start_pos)
         end,
       }
-      -- local kopts = { noremap = true, silent = true }
-      -- vim.api.nvim_set_keymap(
-      --   "n",
-      --   "=",
-      --   [[<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>]],
-      --   kopts
-      -- )
-      -- vim.api.nvim_set_keymap(
-      --   "n",
-      --   "-",
-      --   [[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>]],
-      --   kopts
-      -- )
-      -- vim.api.nvim_set_keymap(
-      --   "n",
-      --   "*",
-      --   [[*<Cmd>lua require('hlslens').start()<CR>]],
-      --   kopts
-      -- )
-      -- vim.api.nvim_set_keymap(
-      --   "n",
-      --   "#",
-      --   [[#<Cmd>lua require('hlslens').start()<CR>]],
-      --   kopts
-      -- )
-      -- vim.api.nvim_set_keymap(
-      --   "n",
-      --   "g*",
-      --   [[g*<Cmd>lua require('hlslens').start()<CR>]],
-      --   kopts
-      -- )
-      -- vim.api.nvim_set_keymap(
-      --   "n",
-      --   "g#",
-      --   [[g#<Cmd>lua require('hlslens').start()<CR>]],
-      --   kopts
-      -- )
+      local opts = { noremap = true, silent = true }
+      vim.api.nvim_set_keymap(
+        "n",
+        "=",
+        [[<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>]],
+        opts
+      )
+      vim.api.nvim_set_keymap(
+        "n",
+        "-",
+        [[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>]],
+        opts
+      )
+      vim.api.nvim_set_keymap(
+        "n",
+        "*",
+        [[*<Cmd>lua require('hlslens').start()<CR>]],
+        opts
+      )
+      vim.api.nvim_set_keymap(
+        "n",
+        "#",
+        [[#<Cmd>lua require('hlslens').start()<CR>]],
+        opts
+      )
+      vim.api.nvim_set_keymap(
+        "n",
+        "g*",
+        [[g*<Cmd>lua require('hlslens').start()<CR>]],
+        opts
+      )
+      vim.api.nvim_set_keymap(
+        "n",
+        "g#",
+        [[g#<Cmd>lua require('hlslens').start()<CR>]],
+        opts
+      )
     end,
   },
   {
