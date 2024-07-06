46b672877d414de2f8a8e4d590bb1dde7f4055d9
Noahlias <yygggg@foxmail.com>
2024-07-06 16:48:59 +0800

    Update config

--- lua/config/plugins/markdown.lua
+++ lua/config/plugins/markdown.lua
@@ -10,29 +10,11 @@ return {
   },
   {
     "OXY2DEV/markview.nvim",
-    -- dir = "~/.config/nvim/dev/markview.nvim",
     ft = { "markdown" },
-    -- name = "markview",
-    -- dev = true,
-    commit = "df26311a451361f90b2fe4af1c738a38f61a6544",
-    enabled = true,
+    submodules = false,
     dependencies = {
       "nvim-tree/nvim-web-devicons",
     },
-    config = function()
-      require("markview").setup()
-    end,
-  },
-  {
-    "MeanderingProgrammer/markdown.nvim",
-    name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
-    enabled = false,
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter", -- Mandatory
-      "nvim-tree/nvim-web-devicons", -- Optional but recommended
-    },
-    config = function()
-      require("render-markdown").setup {}
-    end,
+    opts = {},
   },
 }
