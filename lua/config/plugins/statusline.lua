0eaf94718de7da2715babb72879413e3024bc3d1
Noahlias <yygggg@foxmail.com>
2024-07-06 09:22:13 +0800

    Update

--- lua/config/plugins/statusline.lua
+++ lua/config/plugins/statusline.lua
@@ -1,9 +1,3 @@
--- local current_signature = function()
--- 	if not pcall(require, 'lsp_signature') then return end
--- 	local sig = require("lsp_signature").status_line(50)
--- 	return sig.label .. "🐼" .. sig.hint
--- end
-
 local function lsp()
   local clients = vim.lsp.get_clients()
   local buf = vim.api.nvim_get_current_buf()
