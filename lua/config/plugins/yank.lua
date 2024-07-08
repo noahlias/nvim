add9b3186eba316b4e7535082a2185523e07cdc1
Noahlias <yygggg@foxmail.com>
2024-07-08 10:45:37 +0800

    Updated

--- lua/config/plugins/yank.lua
+++ lua/config/plugins/yank.lua
@@ -6,16 +6,32 @@ return {
     { "kkharji/sqlite.lua", module = "sqlite" },
   },
   config = function()
-    ---TODO: change this to fzf-lua
+    ---TODO: change this to fzf-lua but telescope has more futures
     vim.keymap.set(
       "n",
       "<leader>y",
       ":Telescope neoclip<CR>",
       { noremap = true }
     )
+
+    local function is_whitespace(line)
+      return vim.fn.match(line, [[^\s*$]]) ~= -1
+    end
+
+    local function all(tbl, check)
+      for _, entry in ipairs(tbl) do
+        if not check(entry) then
+          return false
+        end
+      end
+      return true
+    end
     require("neoclip").setup {
       history = 1000,
       enable_persistent_history = true,
+      filter = function(data)
+        return not all(data.event.regcontents, is_whitespace)
+      end,
       keys = {
         telescope = {
           i = {
