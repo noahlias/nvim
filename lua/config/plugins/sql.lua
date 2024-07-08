688d3f7794866080cb7fcd62904be834d6cbd3f5
Noahlias <yygggg@foxmail.com>
2024-07-08 10:56:36 +0800

    Lint Format

--- lua/config/plugins/sql.lua
+++ lua/config/plugins/sql.lua
@@ -1,9 +1,14 @@
 ---@type LazyPluginSpec
 return {
   "kristijanhusak/vim-dadbod-ui",
+  event = "VeryLazy",
   dependencies = {
     { "tpope/vim-dadbod", lazy = true },
-    { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+    {
+      "kristijanhusak/vim-dadbod-completion",
+      ft = { "sql", "mysql", "plsql" },
+      lazy = true,
+    },
   },
   cmd = {
     "DBUI",
