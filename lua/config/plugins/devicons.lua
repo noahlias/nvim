1d75465b57039f8902faab0c8fee17df0eccb3a2
Noahlias <yygggg@foxmail.com>
2024-07-27 09:57:47 +0800

    Update config

--- lua/config/plugins/devicons.lua
+++ lua/config/plugins/devicons.lua
@@ -2,33 +2,31 @@
 return {
   "nvim-tree/nvim-web-devicons",
   event = { "BufNewFile", "BufReadPre" },
-  config = function()
-    require("nvim-web-devicons").setup {
-      override_by_extension = {
-        sh = {
-          icon = "",
-          color = "#89e051",
-          cterm_color = "113",
-          name = "Sh",
-        },
-        norg = {
-          icon = "",
-          color = "#97eefc",
-          name = "Neorg",
-        },
-        ["typ"] = {
-          icon = "𝐭",
-          color = "#1a7682",
-          cterm_color = "30",
-          name = "Typst",
-        },
-        ["gleam"] = {
-          icon = "",
-          color = "#ffaff3",
-          cterm_color = "219",
-          name = "Gleam",
-        },
+  opts = {
+    override_by_extension = {
+      sh = {
+        icon = "",
+        color = "#89e051",
+        cterm_color = "113",
+        name = "Sh",
       },
-    }
-  end,
+      norg = {
+        icon = "",
+        color = "#97eefc",
+        name = "Neorg",
+      },
+      ["typ"] = {
+        icon = "𝐭",
+        color = "#1a7682",
+        cterm_color = "30",
+        name = "Typst",
+      },
+      ["gleam"] = {
+        icon = "",
+        color = "#ffaff3",
+        cterm_color = "219",
+        name = "Gleam",
+      },
+    },
+  },
 }
