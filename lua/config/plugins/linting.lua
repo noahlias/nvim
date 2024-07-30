6b5a46d4554105c2aa6abebc66e9be6084ad951b
Noahlias <yygggg@foxmail.com>
2024-07-31 06:55:59 +0800

    Fix bug about github action lint

--- lua/config/plugins/linting.lua
+++ lua/config/plugins/linting.lua
@@ -7,8 +7,48 @@ return {
     linters_by_ft = {
       python = { "mypy" },
       markdown = { "markdownlint" },
+      yaml = { "github_action_lint" },
     },
-    linters = {},
+    linters = {
+      github_action_lint = {
+        cmd = "actionlint",
+        stdin = true,
+        args = { "-format", "{{json .}}", "-" },
+        ignore_exitcode = true,
+        parser = function(output, bufnr, _)
+          local filepath = vim.api.nvim_buf_get_name(bufnr)
+
+          -- Check if the file is under .github/workflows
+          if not filepath:match "%.github/workflows/.*%.y[a]?ml$" then
+            return {}
+          end
+
+          if output == "" then
+            return {}
+          end
+
+          local decoded = vim.json.decode(output)
+          if decoded == nil then
+            return {}
+          end
+
+          local diagnostics = {}
+          for _, item in ipairs(decoded) do
+            table.insert(diagnostics, {
+              bufnr = bufnr,
+              lnum = item.line - 1,
+              end_lnum = item.line - 1,
+              col = item.column - 1,
+              end_col = item.end_column,
+              severity = vim.diagnostic.severity.WARN,
+              source = "actionlint: " .. item.kind,
+              message = item.message,
+            })
+          end
+          return diagnostics
+        end,
+      },
+    },
   },
   config = function(_, opts)
     local M = {}
