e068849e2d89339eeeec75460ee68b48b683489a
Noahlias <yygggg@foxmail.com>
2024-08-05 00:37:14 +0800

    Update gitgraph

--- lua/config/plugins/git.lua
+++ lua/config/plugins/git.lua
@@ -196,6 +196,17 @@ return {
         timestamp = "%H:%M:%S %d-%m-%Y",
         fields = { "hash", "timestamp", "author", "branch_name", "tag" },
       },
+      hooks = {
+        on_select_commit = function(commit)
+          vim.notify("DiffviewOpen " .. commit.hash .. "^!")
+          vim.cmd(":DiffviewOpen " .. commit.hash .. "^!")
+        end,
+        -- Check diff from commit a -> commit b
+        on_select_range_commit = function(from, to)
+          vim.notify("DiffviewOpen " .. from.hash .. "~1.." .. to.hash)
+          vim.cmd(":DiffviewOpen " .. from.hash .. "~1.." .. to.hash)
+        end,
+      },
     },
     keys = {
       {
@@ -203,7 +214,7 @@ return {
         function()
           require("gitgraph").draw({}, { all = true, max_count = 5000 })
         end,
-        desc = "new git graph",
+        desc = "GitGraph - Draw",
       },
     },
   },
