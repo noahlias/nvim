e1812ff765c7f142486575a5ac120919d98e81be
Noahlias <yygggg@foxmail.com>
2024-08-05 11:39:57 +0800

    Remove actions and use clear-actions for replace

--- lua/config/plugins/misc.lua
+++ lua/config/plugins/misc.lua
@@ -433,25 +433,6 @@ return {
     event = "VeryLazy",
     opts = {},
   },
-  -- {
-  --   "aznhe21/actions-preview.nvim",
-  --   lazy = true,
-  --   init = function()
-  --     vim.api.nvim_create_autocmd("LspAttach", {
-  --       desc = "Setup code action preview",
-  --       callback = function(args)
-  --         local bufnr = args.buf
-  --
-  --         vim.keymap.set({ "n", "v" }, "<leader>la", function()
-  --           require("actions-preview").code_actions()
-  --         end, { buffer = bufnr, desc = "LSP: Code action" })
-  --       end,
-  --     })
-  --   end,
-  --   config = function()
-  --     require("actions-preview").setup {}
-  --   end,
-  -- },
   {
     "mrcjkb/haskell-tools.nvim",
     event = { "BufNewFile *.hs", "BufReadPost *.hs" },
