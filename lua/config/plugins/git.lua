459ad7ce4260a4371f69ee70f7d11283c3fe8e90
Noahlias <yygggg@foxmail.com>
2024-08-11 09:40:52 +0800

    Update vim-flog

--- lua/config/plugins/git.lua
+++ lua/config/plugins/git.lua
@@ -184,62 +184,14 @@ return {
     },
   },
   {
-    "isakbm/gitgraph.nvim",
-    dependencies = { "sindrets/diffview.nvim" },
-    event = { "BufNewFile", "BufReadPre" },
-    opts = {
-      symbols = {
-        merge_commit = "",
-        commit = "",
-        merge_commit_end = "",
-        commit_end = "",
-
-        -- Advanced symbols
-        GVER = "",
-        GHOR = "",
-        GCLD = "",
-        GCRD = "╭",
-        GCLU = "",
-        GCRU = "",
-        GLRU = "",
-        GLRD = "",
-        GLUD = "",
-        GRUD = "",
-        GFORKU = "",
-        GFORKD = "",
-        GRUDCD = "",
-        GRUDCU = "",
-        GLUDCD = "",
-        GLUDCU = "",
-        GLRDCL = "",
-        GLRDCR = "",
-        GLRUCL = "",
-        GLRUCR = "",
-      },
-      format = {
-        timestamp = "%H:%M:%S %d-%m-%Y",
-        fields = { "hash", "timestamp", "author", "branch_name", "tag" },
-      },
-      hooks = {
-        on_select_commit = function(commit)
-          vim.notify("DiffviewOpen " .. commit.hash .. "^!")
-          vim.cmd(":DiffviewOpen " .. commit.hash .. "^!")
-        end,
-        -- Check diff from commit a -> commit b
-        on_select_range_commit = function(from, to)
-          vim.notify("DiffviewOpen " .. from.hash .. "~1.." .. to.hash)
-          vim.cmd(":DiffviewOpen " .. from.hash .. "~1.." .. to.hash)
-        end,
-      },
-    },
-    keys = {
-      {
-        "<leader>gg",
-        function()
-          require("gitgraph").draw({}, { all = true, max_count = 2000 })
-        end,
-        desc = "GitGraph - Draw",
-      },
+    "rbong/vim-flog",
+    lazy = true,
+    init = function()
+      vim.g.flog_enable_extended_chars = 1
+    end,
+    cmd = { "Flog", "Flogsplit", "Floggit" },
+    dependencies = {
+      "tpope/vim-fugitive",
     },
   },
 }
