781d2dabb14b7df03b85cdb951ad470277fbfa2d
Noahlias <yygggg@foxmail.com>
2024-09-01 09:20:34 +0800

    Fix java config

--- lua/config/plugins/jdtls.lua
+++ lua/config/plugins/jdtls.lua
@@ -5,4 +5,60 @@ return {
     "BufRead *.java",
     "BufNewFile *.java",
   },
+  opts = function()
+    -- local jdtls = require "jdtls"
+    local mason = require "mason-registry"
+    local capabilities = require "config.capabilities"
+
+    return {
+      cmd = {
+        vim.fs.joinpath(
+          mason.get_package("jdtls"):get_install_path(),
+          "/bin/jdtls"
+        ),
+      },
+      root_dir = vim.fs.dirname(
+        vim.fs.find({ "gradlew", ".git", "mvnw" }, { upward = true })[1]
+      ),
+
+      handlers = {
+        ["language/status"] = function() end,
+      },
+      settings = {
+        java = {
+          inlayHints = {
+            parameterNames = {
+              enabled = "all",
+            },
+          },
+        },
+      },
+      capabilities = capabilities,
+      init_options = {
+        bundles = {
+          vim.fn.glob(
+            vim.fs.joinpath(
+              mason.get_package("java-debug-adapter"):get_install_path(),
+              "extension/server/com.microsoft.java.debug.plugin-*.jar"
+            ),
+            true
+          ),
+        },
+      },
+    }
+  end,
+  config = function(_, opts)
+    local jdtls = require "jdtls"
+
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "java",
+      desc = "Attach jdtls",
+      callback = function()
+        ---@diagnostic disable-next-line: missing-fields
+        jdtls.start_or_attach(opts, { dap = { hotcodereplace = "auto" } })
+        vim.bo.tabstop = 4
+        vim.opt_local.colorcolumn = "100"
+      end,
+    })
+  end,
 }
