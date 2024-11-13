c36f8024ccfc7b354348bdc7afb0b238718cd04b
Noahlias <yygggg@foxmail.com>
2024-11-13 08:50:43 +0800

    Add kuala more keymaps such as curl to http converter

--- after/ftplugin/http.lua
+++ after/ftplugin/http.lua
@@ -1,24 +1,54 @@
 vim.api.nvim_set_keymap(
   "n",
   "<leader>hu",
-  ":lua require('kulala').jump_prev()<CR>",
+  "<cmd>lua require('kulala').jump_prev()<CR>",
   { noremap = true, silent = true, desc = "Kulala Jump to previous file" }
 )
 vim.api.nvim_set_keymap(
   "n",
   "<leader>he",
-  ":lua require('kulala').jump_next()<CR>",
+  "<cmd>lua require('kulala').jump_next()<CR>",
   { noremap = true, silent = true, desc = "Kulala Jump to next file" }
 )
 vim.api.nvim_set_keymap(
   "n",
   "<leader>hr",
-  ":lua require('kulala').run()<CR>",
+  "<cmd>lua require('kulala').run()<CR>",
   { noremap = true, silent = true, desc = "Kulala Run current file" }
 )
 vim.api.nvim_set_keymap(
   "n",
   "<leader>ht",
-  ":lua require('kulala').toggle_view()<CR>",
+  "<cmd>lua require('kulala').toggle_view()<CR>",
   { noremap = true, silent = true, desc = "Kulala Toggle view" }
+)
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>hi",
+  "<cmd>lua require('kulala').inspect()<CR>",
+  { noremap = true, silent = true, desc = "Inspect the current request" }
+)
+
+vim.api.nvim_buf_set_keymap(
+  0,
+  "n",
+  "<leader>ci",
+  "<cmd>lua require('kulala').from_curl()<cr>",
+  {
+    noremap = true,
+    silent = true,
+    desc = "Paste curl from clipboard as http request",
+  }
+)
+
+vim.api.nvim_buf_set_keymap(
+  0,
+  "n",
+  "<leader>co",
+  "<cmd>lua require('kulala').copy()<cr>",
+  {
+    noremap = true,
+    silent = true,
+    desc = "Copy the current request as a curl command",
+  }
 )
