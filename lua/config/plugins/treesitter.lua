56eb797449798c68455baf42fa7edae4a9a79b9d
Noahlias <yygggg@foxmail.com>
2025-02-23 11:20:15 +0800

    revert

--- lua/config/plugins/treesitter.lua
+++ lua/config/plugins/treesitter.lua
@@ -68,7 +68,10 @@ return {
           -- additional_vim_regex_highlighting = false,
           -- use_languagetree = false,
           disable = function(_, bufnr)
-            return vim.bo.ft == "latex"
+            local buf_name = vim.api.nvim_buf_get_name(bufnr)
+            local file_size =
+              vim.api.nvim_call_function("getfsize", { buf_name })
+            return file_size > 256 * 1024 or vim.bo.ft == "latex"
           end,
         },
         ident = {
