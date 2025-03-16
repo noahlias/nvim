dc2d5f2b5100f83b3a885deee6521029f8ab6221
Noahlias <yygggg@foxmail.com>
2025-03-16 23:06:43 +0800

    Add quicker

--- lua/config/plugins/editor.lua
+++ lua/config/plugins/editor.lua
@@ -340,4 +340,11 @@ return {
       },
     },
   },
+  {
+    "stevearc/quicker.nvim",
+    event = "FileType qf",
+    ---@module "quicker"
+    ---@type quicker.SetupOptions
+    opts = {},
+  },
 }
